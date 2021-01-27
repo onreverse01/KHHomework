a6de69caec9fe168f462843821ef84d60abb86e1
onreverse01 <75775238+onreverse01@users.noreply.github.com>
2021-01-27 11:06:50 +0000

    210127

--- oracle_homework/oracle_chun2.sql
+++ oracle_homework/oracle_chun2.sql
@@ -18,16 +18,15 @@ select class_no,
            count(*)
 from tb_class_professor
 group by class_no
-having count(*)>=2;
+having count(*)>=2
+order by count(*) desc;
 
 --4. 학과별로 과목을 구분했을때, 과목구분이 "전공선택"에 한하여 과목수가 10개 이상인 행의 학과번호, 과목구분(class_type), 과목수를 조회(전공선택만 조회)
-select *
-from tb_class;
-
 select department_no 학과구분,
            class_type 과목구분,
            count(*) 과목수
 from tb_class
 where class_type = '전공선택'
 group by class_type, department_no
-having count(*) >=10;
+having count(*) >=10
+order by 과목수 desc;
