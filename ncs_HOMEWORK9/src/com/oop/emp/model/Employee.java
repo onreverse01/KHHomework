633d15fce7520499c1951de2e240d40bbab855b4
onreverse01 <exploder211@naver.com>
2021-02-01 02:13:54 +0900

    a

--- ncs_HOMEWORK9/src/com/oop/emp/model/Employee.java
+++ ncs_HOMEWORK9/src/com/oop/emp/model/Employee.java
@@ -15,6 +15,52 @@ public class Employee {
 	private String phone;
 	private String address;
 	
+	//키보드 입력 메소드
+	public void empInput() {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("사번: ");
+		empNo = sc.nextInt();
+		
+		sc.nextLine();
+		
+		System.out.print("사원이름: ");
+		empName = sc.nextLine();
+		System.out.print("소속부서: ");
+		dept = sc.nextLine();
+		System.out.print("직급: ");
+		job = sc.nextLine();
+		System.out.print("나이: ");
+		age = sc.nextInt();
+		System.out.print("성별: ");
+		gender = sc.next().charAt(0);
+		System.out.print("급여: ");
+		salary = sc.nextInt();
+		System.out.print("보너스포인트: ");
+		bonusPoint = sc.nextDouble();
+		
+		sc.nextLine();
+		
+		System.out.print("핸드폰: ");
+		phone = sc.nextLine();
+		System.out.print("주소: ");
+		address = sc.nextLine();
+	}
+
+	//출력 메소드
+	public void empOutput() {
+		System.out.println(getEmpNo());
+		System.out.println(getEmpName());
+		System.out.println(getDept());
+		System.out.println(getJob());
+		System.out.println(getAge());
+		System.out.println(getGender());
+		System.out.println(getSalary());
+		System.out.println(getBonusPoint());
+		System.out.println(getPhone());
+		System.out.println(getAddress());
+	}
+	
 	public void setEmpNo(int empNo) {
 		this.empNo = empNo;
 	}
@@ -95,61 +141,6 @@ public class Employee {
 		return address;
 	}
 
-	//키보드 입력 메소드
-	public void empInput() {
-		Scanner sc = new Scanner(System.in);
-		
-		System.out.print("사번: ");
-		empNo = sc.nextInt();
-		
-		sc.nextLine();
-		
-		System.out.print("사원이름: ");
-		empName = sc.nextLine();
-		System.out.print("소속부서: ");
-		dept = sc.nextLine();
-		System.out.print("직급: ");
-		job = sc.nextLine();
-		System.out.print("나이: ");
-		age = sc.nextInt();
-		System.out.print("성별: ");
-		gender = sc.next().charAt(0);
-		System.out.print("급여: ");
-		salary = sc.nextInt();
-		System.out.print("보너스포인트: ");
-		bonusPoint = sc.nextDouble();
-		
-		sc.nextLine();
-		
-		System.out.print("핸드폰: ");
-		phone = sc.nextLine();
-		System.out.print("주소: ");
-		address = sc.nextLine();
-	}
-	
-//	private int empNo;
-//	private String empName;
-//	private String dept;
-//	private String job;
-//	private int age;
-//	private char gender;
-//	private int salary;
-//	private double bonusPoint;
-//	private String phone;
-//	private String address;
-	
-	//출력 메소드
-	public void empOutput() {
-		System.out.println(getEmpNo());
-		System.out.println(getEmpName());
-		System.out.println(getDept());
-		System.out.println(getJob());
-		System.out.println(getAge());
-		System.out.println(getGender());
-		System.out.println(getSalary());
-		System.out.println(getBonusPoint());
-		System.out.println(getPhone());
-		System.out.println(getAddress());
-	}
+
 
 }
